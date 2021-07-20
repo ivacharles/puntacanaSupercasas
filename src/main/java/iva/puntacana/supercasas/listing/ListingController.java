e53d34d9921e382b722776488c711303951c3d1b
Iva Lesperance Charles <Iva.LesperanceCharles@mail.citytech.cuny.edu>
2021-07-20 16:36:41 -0400

    JWT_CSRF Security CRUD

--- src/main/java/iva/puntacana/supercasas/listing/ListingController.java
+++ src/main/java/iva/puntacana/supercasas/listing/ListingController.java
@@ -1,5 +1,6 @@
 package iva.puntacana.supercasas.listing;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -17,19 +18,20 @@ public class ListingController {
         this.listingService = listingService;
     }
 
+    @ApiOperation("this method get all the listings.")
     @GetMapping("/")
-    @PreAuthorize("hasRole('ADMIN')")
     public List<Listing> getListings(){
         System.out.println("getListings is called");
         return listingService.getListings();
     }
+    @ApiOperation("this method get a listing base on the listing id.")
     @GetMapping("/{propertyID}")
-    @PreAuthorize("hasRole('ADMIN')")//	@PreAuthorize("") can be hasRole('ROLE_') hasAnyRole('ROLE_') hasAuthority('permission') hasAnyAuthority('permission')
+    //@PreAuthorize("hasRole('ADMIN')")//	@PreAuthorize("") can be hasRole('ROLE_') hasAnyRole('ROLE_') hasAuthority('permission') hasAnyAuthority('permission')
     public Listing getListingByPropertyID(@PathVariable("propertyID") String propertyID) {
         System.out.println("propertyID is "+propertyID +" this means that getLIsting is called");
         return listingService.getListing(propertyID);
     }
-
+    @ApiOperation("this method add a listing.")
     @PostMapping(path="/", consumes= {"application/json"})
     @PreAuthorize("hasRole('ADMIN')")
     public Listing addListing(@RequestBody Listing listing) {
@@ -38,7 +40,7 @@ public class ListingController {
             return new Listing();
         return listing;
     }
-
+    @ApiOperation("this method update a listing. It takes 2 parameters, listing id and the updated version listing")
     @PutMapping(path=("/{propertyID}"), consumes= {"application/json"})
     @PreAuthorize("hasRole('ADMIN')")
     public Listing updateListing(@PathVariable("propertyID") String propertyID, @RequestBody Listing listing) {
@@ -48,7 +50,8 @@ public class ListingController {
         return listing;
     }
 
-    @PostMapping("/{propertyID}")
+    @ApiOperation("this method delete add a listing base on the listing id.")
+    @DeleteMapping("/{propertyID}")
     @PreAuthorize("hasRole('ADMIN')")//	@PreAuthorize("") can be hasRole('ROLE_') hasAnyRole('ROLE_') hasAuthority('permission') hasAnyAuthority('permission')
     public String deleteListing(@PathVariable("propertyID") String propertyID) {
         System.out.println("this means that deleteListing is called");
--- src/main/java/iva/puntacana/supercasas/security/AppSecurityConfig.java
+++ src/main/java/iva/puntacana/supercasas/security/AppSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,6 +47,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(new JwtUsernameAndPasswordAuthFilter(authenticationManager()))
                 .addFilterAfter(new JwtTokenVerifier2(), JwtUsernameAndPasswordAuthFilter.class)
                 .authorizeRequests()
+
                 .anyRequest()
                 .authenticated();
     }
@@ -58,4 +60,24 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+        .antMatchers("/",
+              "/csrf",
+              "/v2/api-docs",
+              "/swagger-resources/configuration/ui",
+              "/configuration/ui", "/swagger-resources",
+              "/swagger-resources/configuration/security",
+              "/configuration/security", "/swagger-ui.html",
+              "/webjars/**");
+    }
+//.antMatchers("/",
+//              "/csrf",
+//              "/v2/api-docs",
+//              "/swagger-resources/configuration/ui",
+//              "/configuration/ui", "/swagger-resources",
+//              "/swagger-resources/configuration/security",
+//              "/configuration/security", "/swagger-ui.html",
+//              "/webjars/**")
 }
